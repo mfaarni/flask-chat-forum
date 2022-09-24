32e4170fa33cc4cc6e301e2d8ec84f08440d63ed
Aarni <aarni.miikkulainen@helsinki.fi>
2022-09-24 21:10:30 +0300

    heroku ei connectaa

--- db.py
+++ db.py
@@ -4,5 +4,7 @@ from os import getenv
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"].replace("postgres://", "postgresql://",1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
