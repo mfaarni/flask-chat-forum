43e584d23026e4d3d12e2c0ad15a39748c98ed44
Aarni <aarni.miikkulainen@helsinki.fi>
2022-10-09 22:01:26 +0300

    korjattu rekisteröitymistä

--- users.py
+++ users.py
@@ -37,9 +37,9 @@ def register(username, password, role):
         sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)"
         db.session.execute(sql, {"username":username, "password":hash_value, "role":role})
         db.session.commit()
+        return login(username, password)
     else:
         return False
-    return login(username, password)
 
 
 def set_quote(user_id, new_content):
